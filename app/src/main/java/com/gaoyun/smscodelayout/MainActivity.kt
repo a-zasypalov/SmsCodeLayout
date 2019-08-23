806cee62341bcb8fe869e6ca4d9d65078415512c
gaoyun <gaoyundexinmen@gmail.com>
2019-08-23 15:18:06 +0400

    Add sample

--- app/src/main/java/com/gaoyun/smscodelayout/MainActivity.kt
+++ app/src/main/java/com/gaoyun/smscodelayout/MainActivity.kt
@@ -2,11 +2,35 @@ package com.gaoyun.smscodelayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        smsCodeView.setActionText("Action")
+        smsCodeView.setTitleText("Title")
+
+        smsCodeView.setTitleTextColor(resources.getColor(R.color.colorPrimary))
+        smsCodeView.setActionTextColor(resources.getColor(R.color.colorAccent))
+
+        smsCodeView.getCode() //returns string like "1234"
+        smsCodeView.setCode("1234")
+
+        //if true then when user type last number keyboard will hide
+        smsCodeView.hideSoftKeyboardOnLastNumberInput(hide = true, activity = this)
+
+        //action when user click "action" button on the bottom of view
+        smsCodeView.setOnActionClickListener(View.OnClickListener {
+            //do something
+        })
+
+        //action when user click done button on system keyboard
+        smsCodeView.setOnActionDoneClickListener {
+            //do something
+        }
     }
 }
